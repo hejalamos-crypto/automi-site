7bc451a105c7542b58b4af619c0b07f9c500d024
hejalamos-crypto <hejalamos@gmail.com>
2025-10-26 22:00:32 +0100

    Create types.ts

---
+++ app/data/components/app/api/parts/types.ts
@@ -0,0 +1,11 @@
+export interface Parts {
+  id: number;
+  name: string;
+  category: string;
+  price: number;
+  stock: number;
+  brand: string;
+  oem: string;
+  image: string;
+  compatibleModels: string[];
+}
