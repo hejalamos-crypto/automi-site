777c340628298ac880f1da6b79ade776c48c8940
hejalamos-crypto <hejalamos@gmail.com>
2025-10-26 22:10:59 +0100

    Create Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="bg-black text-white py-12 mt-20">
+      <div className="max-w-7xl mx-auto px-4 text-center">
+        <p className="text-sm text-gray-400">© 2025 Automi. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
